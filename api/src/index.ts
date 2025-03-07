1befba9438bb4da0a0fae222604f6c0499ccb0ee
Luiz Machado <luizmaachado@gmail.com>
2025-03-07 14:16:24 -0300

    feat: refactoring main function

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,27 +2,27 @@ import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import compression from 'compression';
-import mongoose from 'mongoose';
 import router from './router';
 import dotenv from 'dotenv';
-dotenv.config();
-const MONGO_URL = process.env.MONGO_URL;
-
-const app = express();
+import { connectDB } from './service/dbConnection';
 
 
-app.use(compression());
-app.use(bodyParser.json());
+async function main() {
+    dotenv.config();
 
-const server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> = http.createServer(app);
+    await connectDB();
 
-server.listen(8080, () => console.log('Server is running on http://localhost:8080'))
-
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error));
-mongoose.connection.once('open', () => console.log('Connected mongo'));
+    const app = express();
 
 
-app.use('/', router());
+    app.use(compression());
+    app.use(bodyParser.json());
+
+    const server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> = http.createServer(app);
+
+    server.listen(8080, () => console.log('Server is running on http://localhost:8080'))
+
+    app.use('/api', router());
+}
+
+main()
